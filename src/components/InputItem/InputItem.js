4e800b9e3a659e0dce806e1e03ba0ac4d2ba232b
shtuchka666 <lyskova@sibmail.com>
2021-07-23 22:50:57 +0700

    lesson25

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<span>InputItem</span>);
+const InputItem = () => (<span>
+  <TextField
+  id="standard-number"
+  label="Новое дело"
+  type="number"
+  InputLabelProps={{
+  shrink: true,
+  }}
+  />
+</span>);
 
 export default InputItem;
