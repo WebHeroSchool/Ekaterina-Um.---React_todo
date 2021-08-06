95474a0e8ab9b96e8d18d8b21368b6cc5c2bd2ac
shtuchka666 <lyskova@sibmail.com>
2021-08-06 20:30:25 +0300

    lesson29

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,11 +5,16 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
-export default function Checkboxes({ value, isDone }) {
+export default function Checkboxes({ value, isDone, onClickDone}) {
 
   return (
     <div className={styles.root}>
-      <label><Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+      <label>
+        <Checkbox
+          checked={isDone}
+          inputProps={{ 'aria-label': 'uncontrolled-checkbox' }}
+          onClick={() => onClickDone(isDone)}
+        />
         <span className={
           classnames({
             [styles.done]: isDone,
