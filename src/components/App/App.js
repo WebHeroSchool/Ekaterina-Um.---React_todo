8c512a7e51c640b1daf96e23f5356b494654c3c7
shtuchka666 <lyskova@sibmail.com>
2021-08-06 19:15:55 +0300

    lesson27

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,32 +5,34 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Дело1',
-      isDone: false
-    },
-    {
-      value: 'Дело2',
-      isDone: false
-    },
-    {
-      value: 'Дело3',
-      isDone: true
-    }
-  ];
+class App extends React.Component {
+     render() {
+       const items = [
+         {
+           value: 'Дело1',
+           isDone: false
+         },
+         {
+           value: 'Дело2',
+           isDone: false
+         },
+         {
+           value: 'Дело3',
+           isDone: true
+         }
+       ];
 
-  return (
-  <div>
-    <div className={styles.title}>Мои дела:</div>
-    <div className={styles.wrap}>
-      <CustomizedInputs />
-      <InputItem />
-      <ItemList items = {items}/>
-      <Footer count={3}/>
-    </div>
-  </div>);
-}
+       return (
+         <div>
+           <div className={styles.title}>Мои дела:</div>
+           <div className={styles.wrap}>
+             <CustomizedInputs />
+             <InputItem />
+             <ItemList items = {items}/>
+             <Footer count={3}/>
+           </div>
+         </div>);
+     }
+};
 
 export default App;
