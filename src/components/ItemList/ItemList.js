e22198d309a84e87ab45ec2dae1eef7b9c445ca3
shtuchka666 <lyskova@sibmail.com>
2021-08-09 17:37:49 +0300

    lesson34

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 
@@ -13,5 +14,11 @@ const ItemList = ({ items, onClickDone, onClickDelete }) => (<div>
     />
   </div>)}
 </div>);
+
+ItemList.propTypes = {
+  items: PropTypes.array.isRequired,
+  onClickDone: PropTypes.func.isRequired,
+  onClickDelete: PropTypes.func.isRequired,
+};
 
 export default ItemList;
